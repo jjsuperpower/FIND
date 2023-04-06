fixed sigma calculation

mymodels.py
@@ -9,6 +9,7 @@ from copy import deepcopy
 
 from transforms import Luminance, Hist_EQ, Brightness_Contrast, Retinex, Blur, HistRemap, DistRemap
 from coco_ds import CocoDataset
+from myutils import ImgUtils
 
 class Model_Wrapper(pl.LightningModule):
     def __init__(self, model, model_type='class', scale=1.0):
@@ -64,8 +65,18 @@ class Model_Wrapper(pl.LightningModule):
         else:
             raise NotImplementedError
 
-        self.log('Pixel Val STD', torch.std(imgs) * 255)
-        self.log('Pixel Val MEAN', torch.mean(imgs) * 255)
+        # mus = torch.zeros(imgs.size()[0:2])
+        # sigmas = torch.zeros(imgs.size()[0:2])
+        
+        # for i in range(imgs.size(0)):
+        #     for j in range(imgs.size(1)):
+        #         mus[i,j] = torch.mean(imgs[i,j])
+        #         sigmas[i,j] = torch.std(imgs[i,j])
+        
+        mu, sigma = ImgUtils.get_mean_std(imgs)
+      
+        self.log('Pixel Val MEAN', mu * 255)
+        self.log('Pixel Val STD', sigma * 255)
         # self.log('Loss', loss)get_loader()
         
     def plot(self, *args, **kwargs):

myutils.py
@@ -12,24 +12,24 @@ class Void(object):
         pass
 
 class View:
-    @staticmethod
-    def show_random_color(x, num_to_show):
-        plt.figure(figsize=(6,2))
-        for i in range(num_to_show):
-            ax = plt.subplot(1, num_to_show, i + 1)
-            plt.imshow(x[np.random.random_integers(0,x.shape[0])])
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
-        plt.show()
+    # @staticmethod
+    # def show_random_color(x, num_to_show):
+    #     plt.figure(figsize=(6,2))
+    #     for i in range(num_to_show):
+    #         ax = plt.subplot(1, num_to_show, i + 1)
+    #         plt.imshow(x[np.random.random_integers(0,x.shape[0])])
+    #         ax.get_xaxis().set_visible(False)
+    #         ax.get_yaxis().set_visible(False)
+    #     plt.show()
 
-    def show_all_color(x):
-        plt.figure()
-        for i in range(len(x)):
-            ax = plt.subplot(1, x.shape[0], i + 1)
-            plt.imshow(x[i])
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
-        plt.show()
+    # def show_all_color(x):
+    #     plt.figure()
+    #     for i in range(len(x)):
+    #         ax = plt.subplot(1, x.shape[0], i + 1)
+    #         plt.imshow(x[i])
+    #         ax.get_xaxis().set_visible(False)
+    #         ax.get_yaxis().set_visible(False)
+    #     plt.show()
         
     @staticmethod
     def chan_f2l(array:np.ndarray) -> np.ndarray:
@@ -306,5 +306,15 @@ class ImgUtils:
                 hist[img[x, y]] += 1
         return hist
     
-    def get_dist(img:torch.Tensor):
-        return torch.mean(img), torch.std(img)
+    @staticmethod
+    def get_mean_std(img:torch.Tensor):       
+        if img.dim() == 2:
+            img = img.unsqueeze(0)
+        if img.dim() == 3:
+            img = img.unsqueeze(0)
+            
+        gray = torch.mean(img.clone(), dim=1)      # convert to grayscale            
+        sigmas = torch.std(gray, dim=(1,2))      # get deviation of each grayscale image
+        mus = torch.mean(gray, dim=0)        # get mean of each grayscale image
+        
+        return torch.mean(mus), torch.mean(sigmas)  # return the average of the means and deviations

transforms.py
@@ -208,18 +208,23 @@ class HistRemap():
 class DistRemap:
     ''' Transform for remapping images to a target distribution '''
     
-    def __init__(self, mu:torch.Tensor, sigma:torch.Tensor):
+    def __init__(self, mu:torch.Tensor=None, sigma:torch.Tensor=None):
         self.mu = mu
         self.sigma = sigma
         
     def __call__(self, img:torch.TensorType) -> torch.Tensor:
         ''' Mu and sigma are in range [0, 1]'''
-        img_mu, img_sigma = torch.mean(img), torch.std(img)
+        img_mu, img_sigma = ImgUtils.get_mean_std(img)
         
-        contrast = (self.sigma / img_sigma)*2
+        if self.mu is None:
+            self.mu = img_mu
+        if self.sigma is None:
+            self.sigma = img_sigma
+        
         brightness = (self.mu - img_mu)
+        contrast = self.sigma / img_sigma
         
-        img = (img- 0.5) * contrast + 0.5 + brightness
+        img = (img - 0.5) * contrast + 0.5 + brightness
                 
         return torch.clip(img, 0, 1)
         