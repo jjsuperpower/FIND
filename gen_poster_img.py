643f8463f07fb81d4bdd50b385cba2ad507f7e9b
jjsuperpower <jjs29356@gmail.com>
2023-04-11 15:16:12 -0500

    generating imgs for poster

---
+++ gen_poster_img.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import Image
+from torchvision import transforms as tf
+
+from transforms import Luminance, Fog, Rain, Hist_EQ, Retinex
+
+SAVE_PATH = 'images/poster_imgs/'
+IMG_FILENAME = 'images/ILSVRC2012_val_00033521.JPEG'
+img_orig = Image.open(IMG_FILENAME)
+
+img_orig_tensor = tf.ToTensor()(tf.Resize((640,640))(img_orig))
+
+img_effects = [Luminance(1/8), Luminance(2), Fog(10), Rain(2, 75)]
+img_effects_names = ['dark', 'overexposed', 'foggy', 'darkrainy']
+
+img_enhance = [Hist_EQ(), Retinex('SSR', 100)]
+img_enhance_names = ['histeq', 'retinex']
+
+
+# Save original image
+img_orig.save(SAVE_PATH + 'original.png')
+
+for i, effect in enumerate(img_effects):
+    img = effect(img_orig_tensor)
+    img_pil = tf.ToPILImage()(img)
+    img_pil.save(SAVE_PATH + f'{img_effects_names[i]}.png')
+    
+    for j, enh in enumerate(img_enhance):
+        img = enh(img)
+        img_pil = tf.ToPILImage()(img)
+        img_pil.save(SAVE_PATH + f'{img_effects_names[i]}_{img_enhance_names[j]}.png')
